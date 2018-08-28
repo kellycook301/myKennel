223cdb8924ad3555abb8372d9e1ed5c75ec636b2
Kelly Cook <kellycook@Kellys-MacBook-Pro.local>
2018-08-28 14:54:30 -0500

    added detail and delete feature for animals and employees

---
+++ src/modules/AnimalManager.js
@@ -0,0 +1,25 @@
+const remoteURL = "http://localhost:5002"
+
+export default Object.create(null, {
+    get: {
+        value: function (id) {
+            return fetch(`${remoteURL}/animals/${id}`).then(e => e.json())
+        }
+    },
+    getAll: {
+        value: function () {
+            return fetch(`${remoteURL}/animals`).then(e => e.json())
+        }
+    },
+    post: {
+        value: function (newAnimal) {
+            return fetch(`${remoteURL}/animals`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(newAnimal)
+            }).then(e => e.json())
+        }
+    },
+})
